f2775f54c20e4d4f3441a3716ca7e37d7d29eb4b
RayanSinghGit <rayansingh10@gmail.com>
2025-03-20 04:48:37 +0000

    Fixed Flask port binding for Render deployment

--- stock_api.py
+++ stock_api.py
@@ -117,5 +117,6 @@ def get_chart():
     return send_file(chart_img, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000, debug=True)
+
 
